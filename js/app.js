44d4f1053f29160ec67e5b0ee176ddc7226cd7fe
Yazan Baker <yazanbaker94@hotmail.com>
2021-02-24 15:17:47 +0200

    added two functions for the IF statement and the document innerhtml

--- js/app.js
+++ js/app.js
@@ -7,22 +7,35 @@ var realAge = prompt("Whats your age? ");
 var balance = prompt("Whats your balance in the bank?" );
 var convertedBalance = parseInt(balance);
 
+
 alert("Your real name is: " + realName);
 alert("Your real age is: " + realAge);
 
-if(convertedBalance == 100 && convertedBalance >= 0) {
-    alert("You are doing well financially");
+function convertedBalanceFunction(balance) {
 
-} else if(convertedBalance < 100) {
+    if(convertedBalance == 100 && convertedBalance >= 0) {
+        alert("You are doing well financially");
+    
+    } else if(convertedBalance < 100) {
+    
+        alert("You are broke");
+    } else if(convertedBalance >= 101) {
+        alert("You are super rich!");
+    } else("Wrong Input! ");
+}
 
-    alert("You are broke");
-} else if(convertedBalance >= 101) {
-    alert("You are super rich!");
-} else("Wrong Input! ");
 
-document.getElementById("guess").innerHTML = "Your real name is: " + realName;
-document.getElementById("guess1").innerHTML = "Your real age is: " + realAge;
-document.getElementById("guess2").innerHTML = "Your final balance is: " + convertedBalance;
+convertedBalanceFunction(balance);
+
+function render(){
+    document.getElementById("guess").innerHTML = "Your real name is: " + realName;
+    document.getElementById("guess1").innerHTML = "Your real age is: " + realAge;
+    document.getElementById("guess2").innerHTML = "Your final balance is: " + convertedBalance;
+    
+}
+render();
+
+
 
 
 confirm("Press OK if you are happy!");
